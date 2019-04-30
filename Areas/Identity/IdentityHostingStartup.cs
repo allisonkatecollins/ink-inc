88c3a41489081b327b1d4a982d4408af7c7d3392
Allison Collins <allisonkatecollins@gmail.com>
2019-04-30 15:57:57 -0500

    first commit, set up file structure

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InkInc.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InkInc.Areas.Identity.IdentityHostingStartup))]
+namespace InkInc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Models/Parlor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InkInc.Models
+{
+    public class Parlor
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Display(Name = "Address")]
+        public string FullAddress
+        {
+            get
+            {
+                return $"{StreetAddress}, {City}, {State}";
+            }
+        }
+
+        [Required]
+        public int OpenTime { get; set; }
+
+        [Required]
+        public int CloseTime { get; set; }
+
+        [Required]
+        public string DaysOpen { get; set; }
+
+        [Display(Name = "Hours of Operation")]
+        public string Hours
+        {
+            get
+            {
+                return $"{OpenTime} - {CloseTime}, {DaysOpen}";
+            }
+        }
+
+        public User User { get; set; }
+    }
+}
