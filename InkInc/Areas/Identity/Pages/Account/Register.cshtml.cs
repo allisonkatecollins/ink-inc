fb683a93e5bd46171be3a466ffcb91f82d1ee16f
Allison Collins <allisonkatecollins@gmail.com>
2019-05-02 10:29:55 -0500

    getting there

--- InkInc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InkInc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -75,7 +75,7 @@ namespace InkInc.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
---
+++ InkInc/Controllers/UserController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InkInc.Data;
+using InkInc.Models;
+
+namespace InkInc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.User.Include(u => u.Parlor);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var User = await _context.User
+                .Include(u => u.Parlor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            return View(User);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            ViewData["ParlorId"] = new SelectList(_context.Parlor, "ParlorId", "City");
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Email,FirstName,LastName,BaselinePricing,PricePerHour,InstagramHandle,Biography,ParlorId")] User User)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(User);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParlorId"] = new SelectList(_context.Parlor, "ParlorId", "City", User.ParlorId);
+            return View(User);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var User = await _context.User.FindAsync(id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParlorId"] = new SelectList(_context.Parlor, "ParlorId", "City", User.ParlorId);
+            return View(User);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Email,FirstName,LastName,BaselinePricing,PricePerHour,InstagramHandle,Biography,ParlorId")] User User)
+        {
+            if (id != User.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(User);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(User.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParlorId"] = new SelectList(_context.Parlor, "ParlorId", "City", User.ParlorId);
+            return View(User);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var User = await _context.User
+                .Include(u => u.Parlor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            return View(User);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var User = await _context.User.FindAsync(id);
+            _context.User.Remove(User);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(string id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
