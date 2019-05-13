7693d8692a1775291fd4985397c2d3c3266f6d19
Allison Collins <allisonkatecollins@gmail.com>
2019-05-13 11:04:56 -0500

    files delete from file structure and database!

--- InkInc/Controllers/PhotosController.cs
+++ InkInc/Controllers/PhotosController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using InkInc.Data;
 using InkInc.Models;
 using Microsoft.AspNetCore.Identity;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace InkInc.Controllers
 {
@@ -15,12 +17,16 @@ namespace InkInc.Controllers
     {
         private readonly ApplicationDbContext _context;
 
+        //needed to access full path to delete photo
+        private readonly IHostingEnvironment _hostEnviro;
+
         //access the currently authenticated user
         private readonly UserManager<User> _userManager;
 
         //inject UserManager service
-        public PhotosController(ApplicationDbContext context, UserManager<User> userManager)
+        public PhotosController(ApplicationDbContext context, UserManager<User> userManager, IHostingEnvironment hostingEnvironment)
         {
+            _hostEnviro = hostingEnvironment;
             _userManager = userManager;
             _context = context;
         }
@@ -83,13 +89,33 @@ namespace InkInc.Controllers
             //get current logged in user
             var user = await _userManager.GetUserAsync(HttpContext.User);
             var photo = await _context.Photo.FindAsync(id);
+
+            //====== delete from file structure ========
+
+            //get photo's file path, use Substring method to return the rest of the string starting at index 2
+            //index at 2 removes the tilde and the first slash
+            //so this method returns wwwroot/images/filename instead of ~/images/filename (full file path instead of relative)
+            //store in variable pathSansTilde, which is /images/
+            //Substring creates a new string minus the first 2 characters - doesn't modify string, but creates new one
+            var pathSansTilde = photo.FilePath.Substring(2);
+            //use same process as we used to save photo in UsersController with Path.Combine
+            var fullPath = Path.Combine(_hostEnviro.WebRootPath, pathSansTilde);
+
+            if (System.IO.File.Exists(fullPath))
+                {
+                   System.IO.File.Delete(fullPath);
+                }   
+            
+            //delete from database
             _context.Photo.Remove(photo);
             await _context.SaveChangesAsync();
+
             //after deleting, redirect to current logged in user's details page
             return RedirectToAction("Details", "Users", new { id = user.Id });
         }
 
-        private bool PhotoExists(int id)
+
+    private bool PhotoExists(int id)
         {
             return _context.Photo.Any(e => e.Id == id);
         }
